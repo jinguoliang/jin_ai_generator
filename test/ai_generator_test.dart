0b592c684a9f8d26cb987bc66e595ee0036cb3f5
jinguol999 <jinguol999@163.com>
2025-09-15 16:56:19 +0800

    test: 添加 AI 生成器测试
    - 新增 ai_generator_test.dart 文件
    - 实现了对 BigModel、Bailian、Qianfan 和 OpenAI 生成器的测试
    - 测试内容包括生成显化语句和处理不同 API 密钥、模型等参数- 设置了较长的超时时间以适应可能的网络延迟

---
+++ test/ai_generator_test.dart
@@ -0,0 +1,63 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+@Timeout(Duration(seconds: 160))
+import 'package:jin_ai_generator/bailian_ai_generator.dart';
+import 'package:jin_ai_generator/big_model_ai_generator.dart';
+import 'package:jin_ai_generator/i_ai_generator.dart';
+import 'package:test/test.dart';
+
+
+void main() {
+  callAiGenerator(AiGenerator aiGenerator) async {
+    final result = await aiGenerator.generate(
+      "说出一些显化语句，每个语句都要包含主谓宾",
+      systemPrompt: "你是一个魔法师",
+      maxTokens: 1024,
+      withThinking: false,
+      jsonResponse: false,
+    );
+    print(result);
+  }
+
+  test('BigModel generate message', () async {
+    await callAiGenerator(
+      AiGenerator.createBigModelWith(
+        apiKey: "your_api_key",
+        model: BigModelAIGenerator.MODEL_glm_z1_flash,
+      ),
+    );
+  });
+
+  /// 阿里的千问模型如果response_format为json_object，但是在提示词里没有 json，就会生成出错，有点不合理呀
+  test('Bailian generate message', () async {
+    await callAiGenerator(
+      AiGenerator.createBailianWith(
+        apiKey: "your_api_key",
+        model: BailianAIGenerator.MODEL_qwen_plus,
+      ),
+    );
+  });
+
+  test('Qianfan generate message', () async {
+    await callAiGenerator(
+      AiGenerator.createQianfanWith(
+        apiKey: "your_api_key",
+        model: 'qwen3-32b',
+      ),
+    );
+  });
+
+  test('OpenAI implement Generator And generate message', () async {
+    await callAiGenerator(
+      AiGenerator.createOpenAIWith(
+        apiKey: "your_api_key",
+        model: BailianAIGenerator.MODEL_qwen_plus,
+        baseUrl: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
+      ),
+    );
+  });
+}
